7834d4052e53fd824fa5f3bfb3f2fc8f960364e3
brightsummit.co <naseemoyebola@gmail.com>
2021-12-24 23:15:56 +0100

    initial commit

---
+++ PaulHudson/PaulHudson/PaulHudsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaulHudsonApp.swift
+//  PaulHudson
+//
+//  Created by IBE on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct PaulHudsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
