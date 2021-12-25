0986648250ad026ec85be541580d44760b5baa42
brightsummit.co <naseemoyebola@gmail.com>
2021-12-25 15:02:38 +0100

    Guess Flag

--- PaulHudson/PaulHudson/GuessFlag.swift
+++ PaulHudson/PaulHudson/GuessFlag.swift
@@ -8,9 +8,69 @@
 import SwiftUI
 
 struct GuessFlag: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+
+   @State private var countries = [AppImage.nigeriaFlag, AppImage.estoniaFlag, AppImage.franceFlag, AppImage.germanyFlag, AppImage.irelandFlag, AppImage.italyFlag, AppImage.polandFlag, AppImage.russiaFlag, AppImage.spainFlag, AppImage.ukFlag, AppImage.usFlag, AppImage.monacoFlag].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack {
+                Color.blue
+                LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack(spacing: 50) {
+                    VStack {
+                        VStack {
+                            Text("Tap the flag of")
+                                .foregroundColor(.white)
+                                .font(.subheadline.weight(.heavy))
+
+                            Text(countries[correctAnswer])
+                                .foregroundColor(.white)
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        .padding()
+                        
+                        ForEach(0..<12) { country in
+                            Button{
+                                flagTapped(country)
+                            } label: {
+                                Image(countries[country])
+                                    .renderingMode(.original)
+                                    .clipShape(Capsule())
+                                    .shadow(radius: 5)
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is ???")
+            }
+        }
     }
+    
+    func flagTapped(_ country: Int) {
+        if country == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+
 }
 
 struct GuessFlag_Previews: PreviewProvider {
@@ -18,3 +78,12 @@ struct GuessFlag_Previews: PreviewProvider {
         GuessFlag()
     }
 }
+
+
+//        LinearGradient(gradient: Gradient(colors: [.pink, .purple, .mint]), startPoint: .top, endPoint: .bottom)
+//            .ignoresSafeArea()
+//        LinearGradient(gradient: Gradient(stops: [
+//            .init(color: .pink, location: 0.2),
+//            .init(color: .purple, location: 0.4),
+//            .init(color: .mint, location: 0.9)
+//        ]), startPoint: .top, endPoint: .bottom)
