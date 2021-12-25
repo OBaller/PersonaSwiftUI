a3b28733b1cdb8f861dcfe439ce0914cc6d2cc74
brightsummit.co <naseemoyebola@gmail.com>
2021-12-25 10:27:19 +0100

    AppImage

---
+++ PaulHudson/PaulHudson/AppImage.swift
@@ -0,0 +1,24 @@
+//
+//  AppImage.swift
+//  PaulHudson
+//
+//  Created by naseem on 25/12/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppImage {
+    public static let nigeriaFlag = "nigeria"
+    public static let estoniaFlag = "estonia"
+    public static let franceFlag = "france"
+    public static let germanyFlag = "germany"
+    public static let irelandFlag = "ireland"
+    public static let italyFlag = "italy"
+    public static let monacoFlag = "monaco"
+    public static let polandFlag = "poland"
+    public static let russiaFlag = "russia"
+    public static let spainFlag = "spain"
+    public static let ukFlag  = "uk"
+    public static let usFlag = "us"
+}
---
+++ PaulHudson/PaulHudson/GuessFlag.swift
@@ -0,0 +1,20 @@
+//
+//  GuessFlag.swift
+//  PaulHudson
+//
+//  Created by naseem on 25/12/2021.
+//
+
+import SwiftUI
+
+struct GuessFlag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuessFlag_Previews: PreviewProvider {
+    static var previews: some View {
+        GuessFlag()
+    }
+}
