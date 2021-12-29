bf38b1b00fb92649c1cdc7c5640bbba78abfd617
brightsummit.co <naseemoyebola@gmail.com>
2021-12-29 01:20:22 +0100

    CustomModifier

---
+++ PaulHudson/PaulHudson/CustomModifier.swift
@@ -0,0 +1,58 @@
+//
+//  CustomModifier.swift
+//  PaulHudson
+//
+//  Created by naseem on 29/12/2021.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+struct CustomModifier: View {
+    var body: some View {
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+}
+
+struct CustomModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomModifier()
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
